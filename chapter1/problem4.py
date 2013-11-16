ed0ebdbd8183053dd0a2079a93fafb9b409a7b50
hahnicity <grehm87@gmail.com>
2013-11-15 23:10:10 -0800

    Adding part b

--- chapter1/problem4.py
+++ chapter1/problem4.py
@@ -20,29 +20,61 @@ as sigma^2 for part a.
 """
 from math import exp, fabs
 
-from numpy import array, transpose
+from numpy import array, var
+
+
+def get_yield_and_prob():
+    """
+    Return the yield and the expectation (probability weight) of each yield
+    """
+    return array([0.7, 1.3]), array([0.5, 0.5])
+
+
+def set_gov_support(p, support):
+    """
+    Given an 1d vector of prices modifies an item if below the price threshold
+    """
+    for i, x in enumerate(p):
+        if x < support:
+            p[i] = support
 
 
 def part_a():
     """
     Compute the variance in price
     """
-    y = array([0.7, 1.3])
-    w = array([0.5, 0.5])  # The expectation (probability weight) of each yield
     a = 1
+    y, w = get_yield_and_prob()
     for _ in range(100):
         a_previous = a
         p = 3 - 2 * a * y
         f = w.dot(p)
         a = 0.5 + 0.5 * f
-        print a, f
         if fabs(a_previous - a) < exp(-8):
             break
-    print a, f, w * p, p
+    print "acreage", a, "variance:", var(p), "expectation", p.dot(w)
+
+
+def part_b():
+    """
+    Computer part a with gov support factored in
+    """
+    a = 1
+    y, w = get_yield_and_prob()
+    for _ in range(100):
+        a_previous = a
+        p = 3 - 2 * a * y
+        set_gov_support(p, 1.0)
+        f = w.dot(p)
+        a = 0.5 + 0.5 * f
+        if fabs(a_previous - a) < exp(-8):
+            break
+    print "acreage", a, "variance:", var(p), "expectation", p.dot(w)
 
 
 def main():
     part_a()
+    part_b()
 
 
 if __name__ == "__main__":
